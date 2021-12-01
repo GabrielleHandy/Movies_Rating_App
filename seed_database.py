7ad2035363dba3802ca9ee55b781ba900930b2f0
gabby <gehandy123@gmail.com>
2021-12-01 19:58:53 +0000

    Finished Part 2

--- seed_database.py
+++ seed_database.py
@@ -31,3 +31,16 @@ for movie in movie_data:
     # TODO: create a movie here and append it to movies_in_db
     new_movie = crud.create_movie(title, overview, release_date, poster_path)
     movies_in_db.append(new_movie)
+
+for n in range(10):
+    email = f'user{n}@test.com'  # Voila! A unique email!
+    password = 'test'
+
+    # TODO: create a user here
+    new_user = crud.create_user(email, password)
+
+    # TODO: create 10 ratings for the user
+    for n in range(10):
+        rand_movie = choice(movies_in_db)
+        rand_score = randint(1,5)
+        crud.create_rating(new_user, rand_movie, rand_score)
